0747708ed8bddefb655f784b542a0859efa6ee19
Brian Davis <turbohappy@gmail.com>
2018-07-10 22:21:28 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,11 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
-  blah = colors.map { |color| exp_color(color) }
+  blah = []
+  for i in 0..colors.size
+    blah.push(exp_color(colors[i]))
+  end
+  # blah = colors.map { |color| exp_color(color) }
   puts blah.join(", ")
 end
 
